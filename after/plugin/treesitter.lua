3efa4deb4c95cec659bd3207882d84bf3ed6ff70
John Bonjour <whitewolfzhang@gmail.com>
2025-07-06 21:46:40 -0700

    treesitter highlight disable for csv

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -29,6 +29,9 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
+    disable = {
+        "csv"
+    },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
