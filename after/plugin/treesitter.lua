3f4b005952d32d6a4420da50e5fd9a6992ec751f
John Bonjour <whitewolfzhang@gmail.com>
2026-06-09 21:51:12 +0800

    remove treesitter auto indent

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -63,6 +63,6 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.treesitter.start()                                    -- highlighting
     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'     -- folds
     vim.wo.foldmethod = 'expr'
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+--    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
   end,
 })
